632b120b95acd95500778e598d88beb16165c351
Subin Sabu <subincs2023@gmail.com>
2025-07-05 16:29:15 +0530

    prepared for deployment on render

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from "dotenv";
 import cors from 'cors';
+import path from 'path';
 
 import { connectDB } from './config/db.js';
 import notesRoutes from "./routes/notesRoutes.js"
@@ -10,17 +11,28 @@ dotenv.config()
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
-// middlewares
-app.use(cors({
+// we are hosting both frontend and backend in same URL using render. So, there are no seperate sources or  cross-origin
+if (process.env.NODE_ENV !== 'production') {
+  app.use(cors({
   origin: "http://localhost:5173"
-}))
+}));
+}
+
 app.use(express.json()) // parses JSON body
 app.use(rateLimiter);
 
 // routes
 app.use("/api/notes", notesRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')))
+
+  app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+})
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
